708a2ede277609206be41611642ec26084302937
tharun-adithiya <tharunadithiyav.s@gmail.com>
2026-01-23 22:55:59 +0530

    Input handler can now handle jump. Character controller is used with PlayerController

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,9 +1,18 @@
+using System;
+using Unity.Android.Gradle.Manifest;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerController : MonoBehaviour
 {
-    [HideInInspector]public PlayerState currentState { get; private set; }
-    [SerializeField] private float moveSpeed=5f;
+    [SerializeField] public CharacterController characterController;
+    [HideInInspector] public PlayerState currentState { get; private set; }
+    [SerializeField] private Transform groundCheckPos;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpSpeed = 5f;
+    [SerializeField] private float gravity = -9.81f;
+    private Vector3 directionToApplyForce;
     private void Start()
     {
         ChangeState(new IdleState(this));
@@ -18,6 +27,7 @@ public class PlayerController : MonoBehaviour
     }
     public void ChangeState(PlayerState newState)
     {
+
         if (currentState == newState) return;
         currentState?.Exit();
         currentState = newState;
@@ -25,6 +35,27 @@ public class PlayerController : MonoBehaviour
     }
     public void Move()
     {
-        transform.position += moveSpeed * Time.deltaTime * Vector3.forward;
+        directionToApplyForce = moveSpeed * Time.deltaTime * Vector3.forward;
+        ApplyGravity();
+        characterController.Move(directionToApplyForce);
+    }
+    public void Jump()
+    {
+        directionToApplyForce = jumpSpeed * Vector3.up;
+        if (characterController.isGrounded)
+        {
+            characterController.Move(directionToApplyForce);
+        }
+    }
+    public void ApplyGravity()
+    {
+        if (characterController.isGrounded && directionToApplyForce.y < 0f)
+            directionToApplyForce.y = -2f;
+
+        directionToApplyForce.y += gravity * Time.deltaTime;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheckPos.position, groundCheckRadius);
     }
 }
